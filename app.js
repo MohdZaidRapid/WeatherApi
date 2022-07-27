ef1212c9668a96375f90eaf53856c183f8c449ea
mohdzaid <mohdzaid@gmail.com>
2022-07-27 18:41:40 +0530

    add

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const request = require("request")
 
 const url = `http://api.weatherstack.com/current?access_key=${PROCESS_API_KEY}`
 
-request({ url: url }, (error, response) => {
+request({ url: url }, (err, response) => {
 
     const data = JSON.parse(response.body)
-    console.log(data)
+    console.log(data) 
 })
