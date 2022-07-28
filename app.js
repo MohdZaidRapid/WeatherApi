312d951b15fa284b929f35744c15c5fe006b2122
mohdzaid <mohdzaid@gmail.com>
2022-07-28 10:26:29 +0530

    add weather api

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const request = require("request")
+require("dotenv").config()
+// console.log(process.env.API_KEY)
 
-const url = `http://api.weatherstack.com/current?access_key=${PROCESS_API_KEY}`
+const url = `http://api.weatherstack.com/current?access_key=${process.env.API_KEY}&query=37.8267,-122.4233`
+
+
 
 request({ url: url }, (error, response) => {
 
     const data = JSON.parse(response.body)
-    console.log(data) 
+    console.log(data.current)
 })
