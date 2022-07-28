7742fe53952bb1a714bbed61bd496032d54ee31c
mohdzaid <mohdzaid@gmail.com>
2022-07-28 16:03:07 +0530

    add reusable function

---
+++ utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request')
+require("dotenv").config()
+
+// console.log(process.env.API_KEY)
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=${process.env.API_KEY}&query=${latitude},${longitude}&units=f`
+
+    request({ url: url, json: true }, (error, response) => {
+        // console.log(response.body.current)
+        if (error) {
+            callback("Unable to connect to weather service", undefined)
+        } else if (response.body.error) {
+            callback("Unable to find location", undefined)
+        }
+        else {
+            callback(undefined, `${response.body.current.weather_descriptions[0]}  it is currently ${response.body.current.temperature} degrees out .There feels like ${response.body.current.feelslike} degree`)
+        }
+    })
+}
+
+module.exports = forecast
