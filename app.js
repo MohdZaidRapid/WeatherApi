8d9c92998ce32d51d98e0ecce95d176e8881a82f
mohdzaid <mohdzaid@gmail.com>
2022-07-28 10:59:07 +0530

    Finish api data response fetching

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const request = require("request")
 require("dotenv").config()
 // console.log(process.env.API_KEY)
 
-const url = `http://api.weatherstack.com/current?access_key=${process.env.API_KEY}&query=37.8267,-122.4233`
+const url = `http://api.weatherstack.com/current?access_key=${process.env.API_KEY}&query=37.8267,-122.4233&units=f`
 
 
 
-request({ url: url }, (error, response) => {
-
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+    // console.log(response.body.current)
+    console.log(`${response.body.current.weather_descriptions[0]}  it is currently ${response.body.current.temperature} degrees out .There feels like ${response.body.current.feelslike} degree`)
 })
+
+
