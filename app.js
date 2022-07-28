8fbf431d6ea6fb7e04d0e743240b29cf5ea9eaf3
mohdzaid <mohdzaid@gmail.com>
2022-07-28 13:13:03 +0530

    made geocode function reusable

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require("request")
+const geoCode = require("./utils/geocode")
 require("dotenv").config()
 // console.log(process.env.API_KEY)
 
@@ -24,17 +25,25 @@ require("dotenv").config()
 // https://api.mapbox.com/geocoding/v5/{endpoint}/{search_text}.json
 
 
-const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=${process.env.GEO_ACCESS_TOKEN}&limit=1`
+// const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=${process.env.GEO_ACCESS_TOKEN}&limit=1`
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-    if (error) {
-        console.log("Unable to connect to location services")
-    } else if (response.body.features.length === 0) {
-        console.log("Unable to find location.Try another search")
-    } else {
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//     if (error) {
+//         console.log("Unable to connect to location services")
+//     } else if (response.body.features.length === 0) {
+//         console.log("Unable to find location.Try another search")
+//     } else {
 
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(latitude, longitude)
-    }
+//         const latitude = response.body.features[0].center[1]
+//         const longitude = response.body.features[0].center[0]
+//         console.log(latitude, longitude)
+//     }
+// })
+
+
+
+
+geoCode("Boston", (error, data) => {
+    console.log("Error", error)
+    console.log("Data", data)
 })
